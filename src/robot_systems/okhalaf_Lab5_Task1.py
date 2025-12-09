0daad713345b590d2ca003d17cfae5a07b3e7721
Omar-Khalaf22 <omar.m.khalaf22@gmail.com>
2025-12-08 20:54:40 -0500

    Add Lab 5 Task 1 HamBot controller

--- src/robot_systems/okhalaf_Lab5_Task1.py
+++ src/robot_systems/okhalaf_Lab5_Task1.py
@@ -159,7 +159,7 @@ def drive_straight(bot, distance_m, rpm=DRIVE_RPM):
     Assumes both wheels command the same RPM.
     """
     bot.reset_encoders()
-    bot.set_left_motor_speed(rpm*1.03)
+    bot.set_left_motor_speed(rpm*1.007)
     bot.set_right_motor_speed(rpm)
 
     while True:
